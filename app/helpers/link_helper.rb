628cfe985e2dbca02320be0a86439e759d5189d4
subverse <subverse@gmx.net>
2010-10-15 18:20:53 +0200

    added link_helper

---
+++ app/helpers/link_helper.rb
@@ -0,0 +1,34 @@
+module LinkHelper
+
+#  def link_param
+
+  def linkTo(text, url, title="")
+   link_to text, url, :title => title
+  end
+
+  def link_to_new_tab(text,url, title="")
+    link_to text, url, {:target => "_blank", :title => title}
+  end
+
+  def link_to_show(url)
+    link_to image_tag("Globe.png", :border => 0), url, :title => "Show"
+  end
+
+  def link_to_edit(url)
+    link_to image_tag("Edit.png", :border => 0), url, :title => "Edit"
+  end
+
+  def link_to_destroy(url)
+    link_to image_tag("Delete.png", :border => 0), url, :confirm => 'Are you sure?', :method => :delete, :title => "Destroy"
+  end
+
+  def link_to_new(url)
+    link_to image_tag("Add.png", :border => 0), url, :title => "New"
+  end
+
+  def link_to_cancel(url)
+    link_to image_tag("Add.png", :border => 0), url, :title => "cancel"
+  end
+
+
+end #end module LinkHelper
